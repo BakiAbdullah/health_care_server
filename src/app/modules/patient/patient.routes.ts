085fb30cd193e814ed0daaa3b0e42e607a204fa3
Baki Abdullah <shatil605@gmail.com>
2025-10-23 23:00:19 +0600

    patient data model created

---
+++ src/app/modules/patient/patient.routes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { PatientController } from "./patient.controller";
+import { UserRole } from "@prisma/client";
+import { auth } from "../../middlewares/auth";
+
+const router = express.Router();
+
+router.get("/", PatientController.getAllFromDB);
+
+router.get("/:id", PatientController.getByIdFromDB);
+
+router.patch("/", auth(UserRole.PATIENT), PatientController.updateIntoDB);
+
+router.delete("/soft/:id", PatientController.softDelete);
+
+export const PatientRoutes = router;
